17f03b70bdac571d318e9800abea305b210b835a
Nyaruko <quanpmph27325@fpt.edu.vn>
2023-03-29 13:32:16 +0700

    Repo DSP

--- src/main/java/com/example/assignment_quanpm_ph27325_sof3011/repository/DongSPRepository.java
+++ src/main/java/com/example/assignment_quanpm_ph27325_sof3011/repository/DongSPRepository.java
@@ -3,17 +3,65 @@ package com.example.assignment_quanpm_ph27325_sof3011.repository;
 import com.example.assignment_quanpm_ph27325_sof3011.entity.DongSP;
 import com.example.assignment_quanpm_ph27325_sof3011.util.HibernateUtil;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 import java.util.List;
+import java.util.UUID;
 
 public class DongSPRepository {
-    private Session session = HibernateUtil.getFACTORY().openSession();
+
     private String fromTable = "FROM DongSP";
+    Transaction transaction = null;
 
     public List<DongSP> getAll() {
-        Query query = session.createQuery(fromTable);
-        return query.getResultList();
+        try (Session session = HibernateUtil.getFACTORY().openSession()) {
+            Query query = session.createQuery(fromTable);
+            return query.getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public DongSP getOne(UUID id) {
+        DongSP dsp = new DongSP();
+        try (Session session = HibernateUtil.getFACTORY().openSession()) {
+            dsp = session.get(DongSP.class, id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dsp;
+    }
+
+    public void addDSP(DongSP dsp) {
+        try (Session session = HibernateUtil.getFACTORY().openSession()) {
+            transaction = session.beginTransaction();
+            session.save(dsp);
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateDSP(DongSP dsp) {
+        try (Session session = HibernateUtil.getFACTORY().openSession()) {
+            transaction = session.beginTransaction();
+            session.merge(dsp);
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteDSP(DongSP dsp) {
+        try (Session session = HibernateUtil.getFACTORY().openSession()) {
+            transaction = session.beginTransaction();
+            session.delete(dsp);
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 //    public static void main(String[] args) {
